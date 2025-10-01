50b944b421ae25971adb70fe2cf3907418ac4461
Bluk1136 <bluk1136@gmail.com>
2025-10-01 17:28:02 -0400

    Update 1

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let placeholder;
+
+function preload(){
+    placeholder = loadImage('placeholder');
+}
+
+
+function setup() 
+{
+	createCanvas(400, 400);
+}
+
+function draw()
+{
+    background(225,0,0)
+}
